0e047872443ac7c51dd183216ba3917fe14bdd9a
Taras <turchenko730@gmail.com>
2024-03-27 16:36:04 +0200

    changed commandValidation test

--- commandValidation.test.js
+++ commandValidation.test.js
@@ -1,8 +1,9 @@
 const { commandValidation } = require("./commandValidation");
 
 describe("commandValidation:", () => {
-  test("should return object like { path, outputFile, format }", () => {
-    const validArgs = commandValidation([
+  let argsArr;
+  beforeEach(() => {
+    argsArr = [
       "C:\\Program Files\\nodejs\\node.exe",
       "C:\\Універ\\Методологія\\lab2\\main",
       "markdown.md",
@@ -10,38 +11,36 @@ describe("commandValidation:", () => {
       "index.html",
       "--format",
       "ansi",
-    ]);
+    ];
+  });
+  test("should return object like { path, outputFile, format }", () => {
+    const validArgs = commandValidation(argsArr);
     expect(validArgs).toHaveProperty("path");
     expect(validArgs).toHaveProperty("outputFile");
     expect(validArgs).toHaveProperty("format");
   });
 
   test("should throw error if wrong argumets provided", () => {
+    argsArr.pop();
+    argsArr.push("wrong format");
     const testFunction = () => {
-      commandValidation([
-        "C:\\Program Files\\nodejs\\node.exe",
-        "C:\\Універ\\Методологія\\lab2\\main",
-        "markdown.md",
-        "--out",
-        "index.html",
-        "--format",
-        "wrongformat",
-      ]);
+      commandValidation(argsArr);
     };
     expect(testFunction).toThrow("wrong format");
   });
 
   test("if format is wrong  then throw error", () => {
+    argsArr = [
+      "C:\\Program Files\\nodejs\\node.exe",
+      "C:\\Універ\\Методологія\\lab2\\main",
+      "markdown.md",
+      "--out",
+      "index.nothtml",
+      "--format",
+      "html",
+    ];
     const testFunction = () => {
-      commandValidation([
-        "C:\\Program Files\\nodejs\\node.exe",
-        "C:\\Універ\\Методологія\\lab2\\main",
-        "markdown.md",
-        "--out",
-        "index.nothtml",
-        "--format",
-        "html",
-      ]);
+      commandValidation(argsArr);
     };
     expect(testFunction).toThrow(
       "you have chosen html format but gave file  name without .html extension"
